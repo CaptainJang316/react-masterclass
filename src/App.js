ad23d6cf8972657aebda5b19d7b4f66548ab1c38
CaptainJang316 <gksrkfkaryghl@gmail.com>
2022-02-07 17:02:50 +0900

    (styled-components) remove duplication

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ const Father = styled.div`
   display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
-`;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
@@ -20,13 +14,15 @@ const Text = styled.span`
   color: white;
 `;
 
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
 function App() {
   return (
     <Father>
-      <BoxOne>
-        <Text>Hello!</Text>
-      </BoxOne>
-      <BoxTwo />
+      <Box bgColor="teal"/>
+      <Circle bgColor="tomato"/>
     </Father>
   );
 }
