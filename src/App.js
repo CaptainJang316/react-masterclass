5f1abce9bc9e4337fda860feabdd9b4a06b66596
CaptainJang316 <gksrkfkaryghl@gmail.com>
2022-02-07 17:19:19 +0900

    (styled-components) 'as' and attr

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Father = styled.div`
-  display: flex;
+  display: block;
 `;
 
 const Box = styled.div`
@@ -18,9 +18,29 @@ const Circle = styled(Box)`
   border-radius: 50px;
 `;
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({required: true, minlength: 10})`
+  background-color: tomato;
+`;
+
+
 function App() {
   return (
-    <Father>
+    <Father as="header">
+      <Btn as="a" href="">Log in</Btn>
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <input />
+      <Btn>Log in</Btn>
       <Box bgColor="teal"/>
       <Circle bgColor="tomato"/>
     </Father>
